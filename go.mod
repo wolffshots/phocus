8464895c457e59b9fb4c27697eea648256eb1e38
jadon <16850875+wolffshots@users.noreply.github.com>
2022-11-07 20:54:21 +0200

    feat: added mqtt and more queuing structure

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module wolffshots/phocus
 
 go 1.18
 
-require github.com/gin-gonic/gin v1.8.1
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.2
+	github.com/gin-gonic/gin v1.8.1
+	github.com/google/uuid v1.3.0
+)
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -10,6 +14,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -19,6 +24,7 @@ require (
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
--- main.go
+++ main.go
@@ -1,34 +1,48 @@
 package main
 
 import (
-    "fmt"                       // for printing
-    "time"                      // for sleeping
-    "net/http"                  // for statuses primarily
+    "fmt"                                       // for printing
+    "time"                                      // for sleeping
+    "net/http"                                  // for statuses primarily
+    "os"
+    "log"
 
-    "github.com/gin-gonic/gin"  // for web server
+    "github.com/gin-gonic/gin"                  // for web server
+    "github.com/google/uuid"                    // for generating UUIDs for commands
+    "github.com/eclipse/paho.mqtt.golang"       // mqtt client
 )
 
 // shape of a message for phocus to interpret and handle queuing of
 type message struct {
-    ID      string  `json:"id"`
-    Command string  `json:"command"`
-    Payload string  `json:"payload"`
+    ID      uuid.UUID   `json:"id"`
+    Command string      `json:"command"`
+    Payload string      `json:"payload"`
 }
 
 // queue of messages seeded with QID to run at startup
 var messages = []message{
-    {ID: "0", Command:"QID", Payload:""},
+    {ID: uuid.New(), Command:"QID", Payload:""},
 }
 
 // loop and add QPGSi x n to the queue as long as it isn't too long
+func queueQPGSn() {
+    for {
+        if(len(messages)<20){
+            messages = append(
+                messages, 
+                message{ID:uuid.New(), Command: "QPGSn", Payload:""}, 
+            )
+        }
+        time.Sleep(30*time.Second)
+    }
+}
 
-
-// endpoints to enqueue new messages
+// enqueue new message manually (requires knowledge of commands and a generated uuid on the request)
 func postMessages(c *gin.Context) {
     var newMessage message
-
+    
     // Call BindJSON to bind the received JSON to
-    // newAlbum.
+    // newMessage.
     if err := c.BindJSON(&newMessage); err != nil {
         return
     }
@@ -38,7 +52,7 @@ func postMessages(c *gin.Context) {
     c.IndentedJSON(http.StatusCreated, newMessage)
 }
 
-// endpoint to view current queue
+// view current queue
 func getMessages(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, messages)
 }
@@ -51,7 +65,7 @@ func getMessageByID(c *gin.Context) {
         c.IndentedJSON(http.StatusOK, messages[0])
     } else {
         for _, a := range messages {
-            if a.ID == id {
+            if a.ID.String() == id {
                 c.IndentedJSON(http.StatusOK, a)
                 return
             }
@@ -59,16 +73,33 @@ func getMessageByID(c *gin.Context) {
         c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
     }
 }
-// endpoint to clear current queue
+
+// clear current queue
 func deleteMessages(c *gin.Context) {
     messages = []message{}
 }
 
 // function to interpret message and run relevant action (command or query)
+// TODO
 
 // function to decode response
+// TODO
 
 // function to check CRC values
+// TODO
+
+// mqtt handlers
+var messagePublishedHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+    fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectionHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+    fmt.Println("Connected")
+}
+
+var connectionLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+    fmt.Printf("Connect lost: %v", err)
+}
 
 func main() {
     router := gin.Default()
@@ -79,9 +110,34 @@ func main() {
 
     // spawns a go-routine which handles web requests
     go router.Run("localhost:8080")
-    
+
+    // start mqtt setup
+  	mqtt.ERROR = log.New(os.Stdout, "[ERROR] ", 0)
+	mqtt.CRITICAL = log.New(os.Stdout, "[CRIT] ", 0)
+	mqtt.WARN = log.New(os.Stdout, "[WARN]  ", 0)
+	mqtt.DEBUG = log.New(os.Stdout, "[DEBUG] ", 0)
+    var broker = "192.168.88.49" // TODO these should be config vars
+    var port = 1883
+    opts := mqtt.NewClientOptions()
+    opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
+    opts.SetClientID("go_phocus_client")
+    opts.SetDefaultPublishHandler(messagePublishedHandler)
+    opts.OnConnect = connectionHandler
+    opts.OnConnectionLost = connectionLostHandler
+    opts.SetPingTimeout(5 * time.Second)
+    client := mqtt.NewClient(opts)
+    if token := client.Connect(); token.Wait() && token.Error() != nil {
+        panic(token.Error())
+    }
+
+	token := client.Publish("go_phocus_client/boot_time", 0, false, time.Now().Format(time.RFC822))
+    token.Wait()
+
     // sleep to make sure web server comes on before polling starts
     time.Sleep(5*time.Second)
+
+    // spawn go-routine to repeatedly enqueue QPGSn commands
+    go queueQPGSn()
 
     // loop to check queue and dequeue index 0, run it process result and wait 30 seconds
     for {
@@ -94,6 +150,6 @@ func main() {
             messages = messages[1:len(messages)]
         }
         // sleep between comms with inverter
-        time.Sleep(30*time.Second)
+        time.Sleep(10*time.Second)
     }
 }
