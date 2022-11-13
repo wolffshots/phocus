a85b3c91a7016bfb8c965a8469594a9d917215ac
jadon <16850875+wolffshots@users.noreply.github.com>
2022-11-13 14:26:07 +0200

    chore: reconfigured install script for asdf

---
+++ install.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/fish
+
+go build -o ./build/phocus main.go
+sudo mv ./build/phocus /usr/bin
+sudo ln -f ./phocus.service /usr/lib/systemd/system/phocus.service
--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-go build -o /usr/bin/phocus main.go
-ln -f ./phocus.service /usr/lib/systemd/system/phocus.service
