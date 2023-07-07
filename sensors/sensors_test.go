90ef55f3cc028555ca15aabf1187f6ab5817c1d9
jadon <16850875+wolffshots@users.noreply.github.com>
2023-07-07 22:41:30 +0200

    test: formatting of sensors

--- sensors/sensors_test.go
+++ sensors/sensors_test.go
@@ -1,37 +1,45 @@
 package phocus_sensors
 
 import (
-	"log"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wolffshots/ha_types/device_classes"
+	"github.com/wolffshots/ha_types/state_classes"
+	"github.com/wolffshots/ha_types/units"
 )
 
 func TestRegister(t *testing.T) {
-	log.Printf(
-		"{\""+
-			"unique_id\":\"%s\",\""+
-			"name\":\"%s\",\""+
-			"state_topic\":\"%s\",\""+
-			"icon\":\"%s\",\""+
-			"value_template\":\"%s\",\""+
-			"unit\":\"%s\",\""+
-			"state_class\":\"%s\",\""+
-			"device_class\":\"%s\",\""+
-			"device\":{\"name\":\"phocus\",\""+
-			"identifiers\":[\"phocus\"],\""+
-			"model\":\"phocus\",\""+
-			"manufacturer\":\"phocus\",\""+
-			"sw_version\":\"%s\"},\""+
-			"force_update\":false"+
-			"}",
-		sensors[0].UniqueId,
-		sensors[0].Name,
-		sensors[0].StateTopic,
-		sensors[0].Icon,
-		sensors[0].ValueTemplate,
-		sensors[0].Unit,
-		sensors[0].StateClass,
-		sensors[0].DeviceClass,
-		"1.1.1",
-	)
-	log.Println(sensors)
+	sensor := Sensor{
+		SensorTopic:   "homeassistant/sensor/phocus/qid_serial/config",
+		UniqueId:      "phocus_qid_serial",
+		Unit:          units.None,
+		StateClass:    state_classes.None,
+		DeviceClass:   device_classes.None,
+		Name:          "QID Serial",
+		ValueTemplate: "{{ value_json.SerialNumber }}",
+		StateTopic:    "phocus/stats/qid",
+		Icon:          "mdi:update",
+	}
+
+	sensorDefinition := Format(sensor, "v0.0.0")
+
+	assert.Equal(t, "{\"unique_id\":\"phocus_qid_serial\",\"name\":\"QID Serial\",\"state_topic\":\"phocus/stats/qid\",\"icon\":\"mdi:update\",\"device\":{\"name\":\"phocus\",\"identifiers\":[\"phocus\"],\"model\":\"phocus\",\"manufacturer\":\"phocus\",\"sw_version\":\"v0.0.0\"},\"force_update\":false, \"value_template\":\"{{ value_json.SerialNumber }}\"}", sensorDefinition)
+
+	sensor = Sensor{
+		SensorTopic:   "homeassistant/sensor/phocus/qpgs2_ac_input_frequency/config",
+		UniqueId:      "phocus_qpgs2_ac_input_frequency",
+		Unit:          units.Frequency,
+		StateClass:    state_classes.Measurement,
+		DeviceClass:   device_classes.Frequency,
+		Name:          "QPGS2 AC Input Frequency",
+		ValueTemplate: "{{ value_json.ACInputFrequency }}",
+		StateTopic:    "phocus/stats/qpgs2",
+		Icon:          "mdi:sine-wave",
+	}
+
+	sensorDefinition = Format(sensor, "v0.0.0")
+
+	assert.Equal(t, "{\"unique_id\":\"phocus_qpgs2_ac_input_frequency\",\"name\":\"QPGS2 AC Input Frequency\",\"state_topic\":\"phocus/stats/qpgs2\",\"icon\":\"mdi:sine-wave\",\"device\":{\"name\":\"phocus\",\"identifiers\":[\"phocus\"],\"model\":\"phocus\",\"manufacturer\":\"phocus\",\"sw_version\":\"v0.0.0\"},\"force_update\":false, \"unit_of_measurement\":\"Hz\", \"state_class\":\"measurement\", \"device_class\":\"frequency\", \"value_template\":\"{{ value_json.ACInputFrequency }}\"}", sensorDefinition)
+
 }
