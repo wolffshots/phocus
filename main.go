a69b1dcd52011139226928db42b5a049b019766c
jadon <16850875+wolffshots@users.noreply.github.com>
2024-04-08 19:29:01 +0200

    chore: bump version

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 	serial "github.com/wolffshots/phocus/v2/serial"     // comms with inverter
 )
 
-const version = "v2.9.8"
+const version = "v2.9.9"
 
 type Configuration struct {
 	Serial struct {
